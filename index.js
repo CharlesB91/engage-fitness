dbb5c0a6dd2d31399d21569104a57bca673bac4a
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-10 16:04:56 +0000

    carousel added with styles and js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $(".sidenav").sidenav();
   });
+
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+  
+  autoplay();
+  function autoplay() {
+    $(".carousel").carousel("next");
+    setTimeout(autoplay, 4000);
+  }
+  
