0237e3aff35e37140e69fef33c236d9c7382480a
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-17 12:13:07 +0000

    home page now rendering

--- fitness_app/views.py
+++ fitness_app/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Post
 
-# Create your views here.
 
-class ContentList(generic.ListView):
-    model = Post
-    queryset = Post.objects.order_by('-created_on')
-    template_name = 'content-list.html'
-    paginate_by = 6
+def home_page(request):
+    return render(request, 'index.html')
