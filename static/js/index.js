e0c57522678a82ea443f3015a905daedc86c7f66
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-17 11:07:11 +0000

    html, css, js templates re added

---
+++ static/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+$(".carousel.carousel-slider").carousel({
+  fullWidth: true,
+  indicators: true,
+});
+
+autoplay();
+function autoplay() {
+  $(".carousel").carousel("next");
+  setTimeout(autoplay, 4000);
+}
