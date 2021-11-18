c99a63082c4677e90a4f3386fb0c0dd35a19bdf6
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-18 11:20:24 +0000

    views page created for content detail page

--- fitness_app/views.py
+++ fitness_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
@@ -7,6 +7,23 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter().order_by('-created_on')
     template_name = 'content-list.html'
     paginate_by = 6
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+
+        return render(
+            request,
+            "content-detail.html",
+            {
+                "post": post,
+                "comments": comments,
+            },
+        )
+
 
 def home_page(request):
     return render(request, 'index.html')
