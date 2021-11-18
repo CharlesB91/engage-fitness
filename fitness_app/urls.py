a9f226b48bb75cb82e533e484af0059a8f953f4f
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-18 11:51:22 +0000

    urls connected to content-detail page. Now need to render data

--- fitness_app/urls.py
+++ fitness_app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('home/', views.PostList.as_view(), name='content'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='content-detail'),
 ]
