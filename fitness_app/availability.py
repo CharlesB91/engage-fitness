bd8b8e3305aa8707578ffd0ce4d126674a66364c
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-11-24 17:40:34 +0000

    logical code written to check avaiable appointments

---
+++ fitness_app/availability.py
@@ -0,0 +1,13 @@
+import datetime
+from .models import Appointment
+
+
+def checkAppointment(start, end):
+    available_list = []
+    bookingList = Appointment.objects
+    for booking in bookingList:
+        if booking.start > end or booking.end < start:
+            available_list.append(True)
+        else:
+            available_list.append(False)
+        return all(available_list)
--- fitness_app/models.py
+++ fitness_app/models.py
@@ -8,24 +8,16 @@ from cloudinary.models import CloudinaryField
 
 
 class Post(models.Model):
-    # Title of work out with max length 200 which is a unique tile so its cant be used again
+
     title = models.CharField(max_length=200, unique=True)
-    # Slug field is used to determine the URL
     slug = models.SlugField(max_length=200, unique=True)
-    # This is the author field who posted the workout. This is a ForeignKey as it relates to an other database table
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="workout_posts")
-    # Date workout was updated which defaults to the current date
     updated_on = models.DateTimeField(auto_now=True)
-    # Workout illustration image which will be stored to cloudinary
     featured_image = CloudinaryField('image', default='placeholder')
-    # Workout instructions/content
     content = models.TextField(default='SOME STRING')
-    # This creates an index for the page
     excerpt = models.TextField(blank=True)
-    # This indicates what date the workout was created on
     created_on = models.DateTimeField(auto_now_add=True)
 
-    
     class Meta:
         # ordered created on field starting with newest first
         ordering = ['-created_on']
@@ -48,6 +40,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
 
 class Appointment(models.Model):
     appointmentUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="appointment_user")
