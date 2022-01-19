6c0ea4f045fa04f634de1c390e128f8e8a66ff5c
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-01-19 17:11:38 +0000

    email details removed

--- engage_fitness/settings.py
+++ engage_fitness/settings.py
@@ -164,7 +164,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
 EMAIL_HOST_USER = 'engage.fitness.training.1@gmail.com' 
-EMAIL_HOST_PASSWORD = 'lykuuevfdqlqlsla' 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_KEY') 
 DEFAULT_FROM_EMAIL = 'engage.fitness.training.1@gmail.com'
 
 
