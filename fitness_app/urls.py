9087a238e0de434ec1b49ff9da24988a36185401
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-01-23 20:19:10 +0000

    background image path amended

--- fitness_app/urls.py
+++ fitness_app/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-# from django.views.i18n import JavaScriptCatalog
+
 
 urlpatterns = [
     path('content-list/', views.PostList.as_view(), name='content-list'),
