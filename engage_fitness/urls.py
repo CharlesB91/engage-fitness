21a665e408cd52dc338b00ed1499a7db43401f11
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2022-01-24 19:18:51 +0000

    further responsive styles added

--- engage_fitness/urls.py
+++ engage_fitness/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from fitness_app.views import home_page 
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  home_page, name='home_page'),
